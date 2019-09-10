0bd26a3abe95b8332cc823cf55c90a04ac901717
Garrett Grube <garrettgrube@Garretts-MBP.home>
2019-09-10 09:54:25 -0400

    refactor code

--- client/src/components/create/ProgressItem.js
+++ client/src/components/create/ProgressItem.js
@@ -12,26 +12,24 @@ class ProgressItem extends Component {
         return (
             <span className="d-flex mb-2">
                 
-                {/* NAME OF ICON */}
+                {/* NAME OF ICON (hyperlink icon to dispatch jump to prompt) */}
                 <a href="#" onClick={()=>{this.props.dispatch(jumpToPrompt(this.props.promptIndex))}}>
                 <i className="material-icons mr-1">{this.props.iconName}</i></a>
                 
                 {/* NAME OF REPORT PROMPT */}
                 <strong className="mr-1">{this.props.promptName}: </strong>
 
-                {/* IS IT COMPLETED? Format conditionally */}
+                {/* IS IT COMPLETED? (Format conditionally) */}
                 <strong className={this.props.completed ? "text-success" : "text-warning"}>
                     {this.props.completed ? "Complete" : "Incomplete"}
                 </strong>
                 
-                {/* EDIT BUTTON. Show only when completed */}
+                {/* EDIT BUTTON (show only when prompt is locked) */}
                 <a className="ml-auto" href="#" onClick={()=>{this.props.dispatch(jumpToPrompt(this.props.promptIndex))}}>{this.props.completed ? "Edit" : ''}</a>
                 
             </span>
         );
     }
-
-
 }
 
 export default connect()(ProgressItem);
